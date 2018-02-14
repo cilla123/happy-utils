860780f960af222e49879865aea9d9870b542122
Ben <892156860@qq.com>
2018-02-14 12:27:34 +0800

    更新入口文件的引入

--- index.js
+++ index.js
@@ -2,10 +2,14 @@ const dateUtils = require('./date')
 const stringUtils = require('./string')
 const numberUtils = require('./number')
 const urlUtils = require('./url')
+const validateUtils = require('./validate')
+const moneyUtils = require('./money')
 
 module.exports = {
     dateUtils,
     stringUtils,
     numberUtils,
     urlUtils,
+    moneyUtils,
+    validateUtils,
 }
